8953c57feb3b470e19631e015fb59557026031a0
Henrique Dirks Bessa <henriquedirksbessa@gmail.com>
2025-02-24 19:57:55 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-# calculadora.py - Versão 1
+# calculadora.py - Versão 2
 
 def somar(a, b):
     return a + b
@@ -6,9 +6,20 @@ def somar(a, b):
 def subtrair(a, b):
     return a - b
 
-print("Calculadora Básica - Versão 1")
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: Divisão por zero"
+
+print("Calculadora - Versão 2")
 a = float(input("Digite o primeiro número: "))
 b = float(input("Digite o segundo número: "))
 
 print("Soma:", somar(a, b))
 print("Subtração:", subtrair(a, b))
+print("Multiplicação:", multiplicar(a, b))
+print("Divisão:", dividir(a, b))
