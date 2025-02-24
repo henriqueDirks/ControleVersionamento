a7729b9db654623f8181eddb9f32b568edaca0d6
Henrique Dirks Bessa <henriquedirksbessa@gmail.com>
2025-02-24 19:55:30 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+# calculadora.py - Versão 1
+
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+print("Calculadora Básica - Versão 1")
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+print("Soma:", somar(a, b))
+print("Subtração:", subtrair(a, b))
